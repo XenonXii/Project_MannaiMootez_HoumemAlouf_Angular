cd9dd53ff59bfb6769331dd302aac60d1cdaf576
XenonXii <xenonxii00@gmail.com>
2025-11-21 00:11:26 +0100

    add about page

--- src/app/front-office/about-page/about-page.ts
+++ src/app/front-office/about-page/about-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about-page.html',
   styleUrl: './about-page.css',
 })
